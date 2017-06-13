d1d10143f42da10b46a8445f50a8c2c678ecd760
Sergio Gayarre Garasa <sgayarre@antevenio.com>
2017-06-13 17:52:07 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication2;
+using Newtonsoft.Json; //install JSON.NET via nuget
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //get request
+
+            string email = "soa@gmail.com";
+            string url = "http://www.mdirector.com/api_contact?email="+email;
+            string consumerKey = "52216";
+            string secretKey = "Qdl0AVLxIk";
+            string contentType = "application/x-www-form-urlencoded";
+
+
+            ServiceProvider ServiceProvider = new ServiceProvider(url, consumerKey, secretKey, "PLAINTEXT");
+            string resultGet = ServiceProvider.GetData(contentType);
+
+            var joGet = Newtonsoft.Json.Linq.JObject.Parse(resultGet);
+
+            string resultado = joGet["response"].ToString();
+
+            if (resultado == "ok") {
+                Console.Out.WriteLine("Obtained user ok");
+                Console.Out.WriteLine(resultGet);
+                Console.Read();
+            } else {
+                Console.Out.WriteLine("Failed obtaining user");
+                Console.Out.WriteLine(resultGet);
+                Console.Read();
+            }
+            
+
+            //post request
+
+            /*
+            string url = "http://www.mdirector.com/api_contact";
+            string consumerKey = "52216";
+            string secretKey = "Qdl0AVLxIk";
+            string contentType = "application/x-www-form-urlencoded";
+
+           
+            ServiceProvider ServiceProvider = new ServiceProvider(url, consumerKey, secretKey, "PLAINTEXT");
+
+
+            string data = "listId=1&name=Soa&surname=Gya&email=soa@gmail.com";
+
+
+            string result = ServiceProvider.PostData(contentType, data);
+
+            var jo = Newtonsoft.Json.Linq.JObject.Parse(result);
+
+            string resultado = jo["response"].ToString();
+            if (resultado == "ok") {
+                Console.Out.WriteLine("Inserted user ok");
+                Console.Out.WriteLine(result);
+                Console.Read();
+            } else {
+
+                Console.Out.WriteLine("Failed Inserting new user");
+                Console.Out.WriteLine(result);
+                Console.Read();
+            }
+
+            */
+
+            //delete request
+            
+            /*
+            string url = "http://www.mdirector.com/api_contact";
+            string consumerKey = "52216";
+            string secretKey = "Qdl0AVLxIk";
+            string contentType = "application/x-www-form-urlencoded";
+
+
+            ServiceProvider ServiceProvider = new ServiceProvider(url, consumerKey, secretKey, "PLAINTEXT");
+
+
+            string data = "conId=1&email=sergiogayarre@gmail.com&unsubscribe=true&reason=baja";
+
+
+            string responseDelete = ServiceProvider.DeleteData(contentType, data);
+
+
+            var joDelete = Newtonsoft.Json.Linq.JObject.Parse(responseDelete);
+
+            string resultDeleted = joDelete["response"].ToString();
+
+            if (resultDeleted == "ok") {
+                Console.Out.WriteLine("Unsubscribed user ok");
+                Console.Out.WriteLine(responseDelete);
+                Console.Read();
+            } else {
+
+                Console.Out.WriteLine("Failed unsubscribing user");
+                Console.Out.WriteLine(responseDelete);
+                Console.Read();
+            }
+
+            */
+
+              //update request
+            
+            /*
+            string url = "http://www.mdirector.com/api_contact";
+            string consumerKey = "52216";
+            string secretKey = "Qdl0AVLxIk";
+            string contentType = "application/x-www-form-urlencoded";
+
+
+            ServiceProvider ServiceProvider = new ServiceProvider(url, consumerKey, secretKey, "PLAINTEXT");
+
+            string data = "listId=1&conId=10&email=soa@gmail.com&name=paquito";
+            string responseUpdate = ServiceProvider.PutData(contentType, data);
+
+            var joUpdate = Newtonsoft.Json.Linq.JObject.Parse(responseUpdate);
+            string resultUpdated = joUpdate["response"].ToString();
+
+            if (resultUpdated == "ok") {
+                Console.Out.WriteLine("Updating user ok");
+                Console.Out.WriteLine(responseUpdate);
+                Console.Read();
+            } else {
+                Console.Out.WriteLine("Failed updating user");
+                Console.Out.WriteLine(responseUpdate);
+                Console.Read();
+            }
+            
+            */
+         
+
+           
+            
+
+        }
+    }
+}
---
+++ ServiceProvider.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using OAuth.Net.Common;
+using OAuth.Net.Components;
+
+namespace ConsoleApplication2
+{
+
+        public class ServiceProvider
+        {
+            private const int RequestTimeOut = 1000 * 60 * 10;
+            private readonly string _consumerKey;
+            private readonly string _consumerSecret;
+            private readonly string _signType;
+            private readonly Uri _serviceProviderUri;
+            private static readonly ISigningProvider SigningProvider = new HmacSha1SigningProvider();
+            private static readonly INonceProvider NonceProvider = new GuidNonceProvider();
+            private string[] validSignType = { "HMAC-SHA1", "PLAINTEXT" };
+
+            public ServiceProvider(string endpoint, string consumerKey, string consumerSecret, string signType)
+            {
+                _consumerKey = consumerKey;
+                _consumerSecret = consumerSecret;
+                _serviceProviderUri = new Uri(endpoint);
+                
+                // check if sign is valid
+                var isValidSign = Array.Exists(validSignType, element => element == signType);
+
+                if (isValidSign == false) 
+                    throw new Exception(string.Format("Error, the signType must be either HMAC-SHA1 or PLAINTEXT"));
+
+                _signType = signType;
+
+            }
+
+            private HttpWebRequest GenerateRequest(string contentType, string requestMethod)
+            {
+                var ts = UnixTime.ToUnixTime(DateTime.Now);
+                var param = new OAuthParameters()
+                {
+                    ConsumerKey = _consumerKey,
+                    SignatureMethod = _signType,
+                    Version = Constants.Version1_0,
+                    Nonce = NonceProvider.GenerateNonce(ts),
+                    Timestamp = ts.ToString(),
+                };
+
+                //Generate Signature Hash
+                // var signatureBase = SignatureBase.Create(requestMethod.ToUpper(), _serviceProviderUri, param);
+                //Set Signature Hash as one of the OAuth Parameter
+                //param.Signature = SigningProvider.ComputeSignature(signatureBase, _consumerSecret, null);
+
+                if (_signType == "HMAC-SHA1")
+                {
+                    var signatureBase = SignatureBase.Create(requestMethod.ToUpper(), _serviceProviderUri, param);
+                    //Set Signature Hash as one of the OAuth Parameter
+                    param.Signature = SigningProvider.ComputeSignature(signatureBase, _consumerSecret, null);
+                }
+
+                if (_signType == "PLAINTEXT")
+                {
+                    param.Signature = _consumerSecret + "&";
+                }
+        
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(_serviceProviderUri);
+                httpWebRequest.Method = requestMethod;
+                httpWebRequest.ContentType = contentType;
+                //httpWebRequest.Timeout = RequestTimeOut;
+                //Add the OAuth Parameters to Authorization Header of Request
+                httpWebRequest.Headers.Add(Constants.AuthorizationHeaderParameter, param.ToHeaderFormat());
+                return httpWebRequest;
+            }
+
+            public string GetData(string contentType)
+            {
+                var request = GenerateRequest(contentType, WebRequestMethods.Http.Get);
+                return GetRequestResponse(request);
+            }
+
+            public string PostData(string contentType, string data)
+            {
+                var request = GenerateRequest(contentType, WebRequestMethods.Http.Post);
+                var bytes = Encoding.ASCII.GetBytes(data);
+                request.ContentLength = bytes.Length;
+                if (bytes.Length > 0)
+                {
+                    using (var requestStream = request.GetRequestStream())
+                    {
+                        if (!requestStream.CanWrite) throw new Exception("The data cannot be written to request stream");
+                        try
+                        {
+                            requestStream.Write(bytes, 0, bytes.Length);
+                        }
+                        catch (Exception exception)
+                        {
+                            throw new Exception(string.Format("Error while writing data to request stream - {0}", exception.Message));
+                        }
+                    }
+                }
+                return GetRequestResponse(request);
+            }
+
+        public string PutData(string contentType, string data)
+        {
+            var request = GenerateRequest(contentType, "PUT");
+            var bytes = Encoding.ASCII.GetBytes(data);
+            request.ContentLength = bytes.Length;
+
+            if (bytes.Length > 0)
+            {
+                using (var requestStream = request.GetRequestStream())
+                {
+                    if (!requestStream.CanWrite) throw new Exception("The data cannot be written to request stream");
+                    try
+                    {
+                        requestStream.Write(bytes, 0, bytes.Length);
+                    }
+                    catch (Exception exception)
+                    {
+                        throw new Exception(string.Format("Error while writing data to request stream - {0}", exception.Message));
+                    }
+                }
+            }
+            return GetRequestResponse(request);
+        }
+
+        public string DeleteData(string contentType, string data)
+            {
+                var request = GenerateRequest(contentType, "DELETE");
+                var bytes = Encoding.ASCII.GetBytes(data);
+                request.ContentLength = bytes.Length;
+
+                if (bytes.Length > 0)
+                {
+                    using (var requestStream = request.GetRequestStream())
+                    {
+                        if (!requestStream.CanWrite) throw new Exception("The data cannot be written to request stream");
+                        try
+                        {
+                            requestStream.Write(bytes, 0, bytes.Length);
+                        }
+                        catch (Exception exception)
+                        {
+                            throw new Exception(string.Format("Error while writing data to request stream - {0}", exception.Message));
+                        }
+                    }
+                }
+                return GetRequestResponse(request);
+            }
+
+            private static string GetRequestResponse(HttpWebRequest httpWebRequest)
+            {
+                if (httpWebRequest == null) throw new ArgumentNullException("httpWebRequest");
+                string responseString = null;
+                try
+                {
+                    using (var response = (HttpWebResponse) httpWebRequest.GetResponse())
+                    {
+                        using (var responseStream = response.GetResponseStream())
+                        {
+                            if (responseStream != null)
+                            {
+                                var reader = new StreamReader(responseStream);
+                                responseString = reader.ReadToEnd();
+                                reader.Close();
+                                responseStream.Close();
+                            }
+                        }
+                        response.Close();
+                    }
+                }
+                catch (WebException webException)
+                {
+                    throw new Exception(string.Format("WebException while reading response - {0}", webException.Message));
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception(string.Format("Unhandled exception while reading response - {0}", exception.Message));
+                }
+                return responseString;
+            }
+
+        }
+    }
+
+
